70bc3839054a111ee16eb4a4c4d201730fd91a8a
jaewon <jaewon1676@daum.net>
2022-03-23 14:42:47 +0900

    최솟값 구하기

--- section1/5.js
+++ section1/5.js
@@ -9,14 +9,18 @@
 ▣ 출력예제 1
 2 */
 
+// function solution(arr){
+
+
+//     arr.sort(function(a, b){
+//         return a - b;
+//     });
+//     answer = arr[0]
+//     return answer
+// }
+
 function solution(arr){
-
-
-    arr.sort(function(a, b){
-        return a - b;
-    });
-    answer = arr[0]
-    return answer
+    return Math.min(...arr)
 }
 arr = [5, 3, 7, 11, 2, 15, 17]
 console.log(solution(arr))
